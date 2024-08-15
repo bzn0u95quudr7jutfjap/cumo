111989192573f8247ba627c7db79c0d43cdc5d68
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-08-15 11:45:07 +0200

    server usa le stack

--- inc/server.h
+++ inc/server.h
@@ -2,5 +2,15 @@
 #define __CUMO_SERVER__
 
 #include <types.h>
+#include <stack.h>
+
+typedef struct {
+  u32 id;
+  u32 path_len;
+} metadata;
+
+declare_template_stack_type(metadata, stack_metadata);
+declare_template_stack_type(char, string);
+declare_template_stack_type(string, stack_string);
 
 #endif //__CUMO_SERVER__
--- src/server.c
+++ src/server.c
@@ -2,20 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  u32 next_id;
+  stack_metadata metadata;
+  stack_string filepaths;
+} server_state;
+
+server_state state = {
+    .next_id = 0, .metadata = newstack, .filepaths = newstack};
+
 u32 next_id;
 u32 files_count;
-
-typedef struct {
-  u32 id;
-  u32 path_len;
-} metadata;
 
 // TODO replace with a stack
 metadata *files_metadata = NULL;
 char **files_names = NULL;
 
-u0 read_database() {
-  FILE *db = fopen("database.cumo", "rb");
+u0 read_database(FILE *db) {
+  fseek(db, 0, SEEK_SET);
   fread(&next_id, sizeof(next_id), 1, db);
   fread(&files_count, sizeof(files_count), 1, db);
   files_metadata = malloc(sizeof(*files_metadata) * files_count);
@@ -27,4 +31,21 @@ u0 read_database() {
     files_names[i] = malloc(sizeofchar * len);
     fread(files_names[i], sizeofchar, len, db);
   }
+}
+
+u0 init_database() {
+  FILE *db = fopen("database.cumo", "ab+");
+  if (db == NULL) {
+    goto exit;
+  }
+  fseek(db, 0, SEEK_END);
+  if (ftell(db) == 0) {
+    u32 bf[1] = {0};
+    fwrite(bf, sizeof(*bf), 1, db);
+  }
+
+  read_database(db);
+
+exit:
+  fclose(db);
 }
