d32d3f92159264a66953235447569a2c33d9804e
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-08-17 12:23:19 +0200

    server rewrite

--- inc/server.h
+++ inc/server.h
@@ -1,16 +1,16 @@
 #ifndef __CUMO_SERVER__
 #define __CUMO_SERVER__
 
-#include <stack.h>
-#include <types.h>
-
-typedef struct {
-  u32 id;
-  u32 path_len;
-} metadata;
-
-declare_template_stack_type(metadata, stack_metadata);
-declare_template_stack_type(char, string);
-declare_template_stack_type(string, stack_string);
+//#include <types.h>
+//#include <stack.h>
+//
+//typedef struct {
+//  u32 id;
+//  u32 path_len;
+//} metadata;
+//
+//declare_template_stack_type(metadata, stack_metadata);
+//declare_template_stack_type(char, string);
+//declare_template_stack_type(string, stack_string);
 
 #endif //__CUMO_SERVER__
--- src/server.c
+++ src/server.c
@@ -1,73 +1,32 @@
+#include "stack.h"
+#include "types.h"
 #include <server.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+declare_template_stack_type(char, string);
 
 typedef struct {
-  u32 next_id;
-  stack_metadata metadata;
-  stack_string paths;
+  u32 id;
+  string path;
+} metadata;
+
+declare_template_stack_type(metadata, stack_metadata);
+
+typedef struct {
+  u32 autoincrement;
+  stack_metadata files;
 } server_state;
 
-server_state state = {.next_id = 0, .metadata = new_stack, .paths = new_stack};
+server_state state = {};
 
-u32 next_id;
-u32 files_count;
+void fread_char(char *c, FILE *db) { fread(c, sizeof(*c), 1, db); }
 
-// TODO replace with a stack
-metadata *files_metadata = NULL;
-char **files_names = NULL;
-
-u0 read_database(FILE *db) {
-  fseek(db, 0, SEEK_SET);
-  u32 next = 0, count = 0;
-  fread(&next, sizeof(next), 1, db);
-  fread(&count, sizeof(count), 1, db);
-
-  state.next_id = next;
-  resize_stack_metadata(&(state.metadata), count);
-  resize_stack_string(&(state.paths), count);
-
-  metadata *data = state.metadata.data;
-  fread(data, sizeof(*data), count, db);
-
-  for (u32 i = 0; i < count; i++) {
-    u32 len = data[i].path_len;
-    string *name = at_stack_string(&(state.paths), i);
-    resize_string(name, len);
-    fread(name->data, sizeof(*(name->data)), len, db);
-  }
+void fread_metadata(metadata *md, FILE *db) {
+  fread(&(md->id), sizeof(md->id), 1, db);
+  fread_string(&(md->path), fread_char, db);
 }
 
-u0 write_database(FILE *db) {
-  fseek(db, 0, SEEK_SET);
-  u32 next = state.next_id, count = state.metadata.size;
-  for (u32 i = 0; i < count; i++) {
-    at_stack_metadata(&(state.metadata), i)->path_len =
-        at_stack_string(&(state.paths), i)->size;
-  }
-  fwrite(&next, sizeof(next), 1, db);
-  fwrite(&count, sizeof(count), 1, db);
-  stack_metadata *data = &(state.metadata);
-  fwrite(data->data, sizeof(*(data->data)), count, db);
-  for (u32 i = 0; i < count; i++) {
-    string *name = at_stack_string(&(state.paths), i);
-    fwrite(name->data, sizeof(*(name->data)), name->size, db);
-  }
-}
-
-u0 init_database() {
-  FILE *db = fopen("database.cumo", "ab+");
-  if (db == NULL) {
-    goto exit;
-  }
-  fseek(db, 0, SEEK_END);
-  if (ftell(db) == 0) {
-    u32 bf[1] = {0};
-    fwrite(bf, sizeof(*bf), 1, db);
-  }
-
-  read_database(db);
-
-exit:
-  fclose(db);
+void fread_server_state(FILE *db) {
+  fread(&(state.autoincrement), sizeof(state.autoincrement), 1, db);
+  fread_stack_metadata(&(state.files), fread_metadata, db);
 }
