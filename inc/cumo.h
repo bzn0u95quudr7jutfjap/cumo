5c6b3480989e096924dd2bbd315ab44a280713f4
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-08-18 14:46:19 +0200

    Squashed prototipo-cumo-sockets
    commit afc23559718e196fa74ce97cee3a98aeb641fec5
    Author: bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
    Date:   Sun Aug 18 14:25:31 2024 +0200
    
        cumo sockets
    
    commit 547c4067ac7f2dc8c909cc5c530f554ab5b1b7ba
    Author: bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
    Date:   Sun Aug 18 14:19:09 2024 +0200
    
        cumo networking wrapper
    
    commit 5137377bb8e16428187df0be15a1192c525a5be7
    Author: bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
    Date:   Sun Aug 18 12:04:43 2024 +0200
    
        code example from gfg

--- inc/cumo.h
+++ inc/cumo.h
@@ -3,6 +3,14 @@
 
 #include "types.h"
 
+typedef struct socket_type socket_type;
+typedef void *(*socket_fn)(socket_type *, void *);
+
+u0 socket_functor_cumo(u32 d, u32 t, u32 p, socket_fn fn, void *a);
+u0 bind_cumo(socket_type *sock, u32 addr, u32 port);
+u0 listen_cumo(socket_type *sock, u32 nconnection);
+u0 accept_functor_cumo(socket_type *sock, socket_fn fn, void *a);
+
 u32 main_server();
 u32 main_client();
 
---
+++ src/cumo_sockets.c
@@ -0,0 +1,59 @@
+#include "types.h"
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h> // read(), write(), close()
+
+#include "cumo.h"
+
+struct socket_type {
+  i32 descriptor;
+  union {
+    struct sockaddr_in _addr;
+    struct sockaddr addr;
+  };
+  u32 addr_len;
+};
+
+socket_type socket_cumo(u32 domain, u32 type, u32 protocol) {
+  i32 sock = socket(domain, type, protocol);
+  if (sock == -1) {
+    exit(1);
+  }
+  return (socket_type){.descriptor = sock, ._addr = {.sin_family = domain}};
+}
+
+u0 bind_cumo(socket_type *sock, u32 addr, u32 port) {
+  sock->_addr.sin_addr.s_addr = htonl(addr);
+  sock->_addr.sin_port = htons(port);
+  if (bind(sock->descriptor, &(sock->addr), sizeof(sock->addr)) != 0) {
+    exit(2);
+  }
+}
+
+u0 listen_cumo(socket_type *sock, u32 nconnection) {
+  if (listen(sock->descriptor, nconnection) != 0) {
+    exit(3);
+  }
+}
+
+socket_type accept_cumo(socket_type *sock) {
+  socket_type conn = {};
+  conn.addr_len = sizeof(conn.addr);
+  conn.descriptor = accept(sock->descriptor, &(conn.addr), &(conn.addr_len));
+  return conn;
+}
+
+u0 socket_functor_cumo(u32 d, u32 t, u32 p, socket_fn fn, void *a) {
+  socket_type sock = socket_cumo(d, t, p);
+  (u0) fn(&sock, a);
+  close(sock.descriptor);
+}
+
+u0 accept_functor_cumo(socket_type *sock, socket_fn fn, void *a) {
+  socket_type conn = accept_cumo(sock);
+  (u0) fn(&conn, a);
+  close(conn.descriptor);
+}
